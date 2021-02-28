08f99aa30aa18c2c3fff6ae0789137b7352cf179
Felipe Bona <felipechiodinibona@hotmail.com>
2021-02-28 12:44:49 -0300

    Fazendo coisas

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import Meta from 'vue-meta'
+
 
 Vue.config.productionTip = false
+
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+Vue.use(Meta)
+
+
 
 new Vue({
   router,
---
+++ src/views/WorkWithUs.vue
@@ -0,0 +1,78 @@
+<template>
+    <div class="container-fluid">
+
+        <div class="row mb-2" v-for="(job, key) in jobList" :key="key">
+            <div class="col-auto border border-dark rounded">
+                <p>Nome: {{ job.name }}</p>
+                <p>Atividades: {{ job.activities }}</p>
+                <p>Cidade: {{ job.address.city }}</p>
+                <p>Diferenciais: {{ job.differential }}</p>
+                <p>Requisitos: {{ job.requirements }}</p>
+            </div>
+        </div>
+
+        <div class="row d-flex justify-content-around align-items-end bg-info">
+
+            <div class="col-auto">
+                <span>dawdwad</span>
+            </div>
+            <div class="col-auto">
+                <span>dawdwad</span>
+                
+            </div>
+            <div class="col-auto">
+                <span>dawdwad</span>
+                
+            </div>
+            <div class="col-auto">
+                
+                <span>dawdwad</span>
+            </div>
+
+        </div>
+
+
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+import Footer from '../components/Footer'
+
+export default {
+    metaInfo: {
+        title: 'Trabalhe Conosco',
+    },
+
+    components: {
+        Footer,
+    },
+
+    mounted() {
+        this.reloadList()
+    },
+
+    data: () => {
+        return {
+
+            jobList: []
+
+        }
+    },  
+
+    methods: {
+
+        reloadList() {
+            const vm = this
+            axios({
+                method: 'GET',
+                url: 'http://localhost:3000/job',
+            }).then((response) => {
+                this.jobList = response.data
+            })
+        },
+
+    }
+
+}
+</script>
